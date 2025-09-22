b0cc0b64d53c879b68e6ffbab7d705c130004e8c
AJ-Wiese <awiese0409@gmail.com>
2025-09-22 10:17:08 -0500

    unchanged version 1 code

---
+++ main.cpp
@@ -0,0 +1,38 @@
+/***************************************************************************************
+* Title: Math Tutor V1
+ * Author: Justin Kottwitz, AJ Wiese
+ * Date: 2025.09.05
+ * GitHub URL: https://github.com/JustinKottwitz/MathTutorV1
+ * Description: Super basic math tutor that doesn't check the answer.
+ **************************************************************************************/
+#include <iostream> //Needed for cout and cin
+using namespace std;
+
+int main() {
+    string userName;
+    int answer;
+
+    cout << "------------------------" << endl;
+    cout << "Math Tutor Version 1!" << endl;
+    cout << "------------------------" << endl;
+
+    cout << "Fun Math Facts:" << endl;
+    cout << "   *All math teachers have cats." << endl;
+    cout << "   *Circles are as easy as pi." << endl;
+    cout << "   *Do NOT try dividing by zero at home." << endl;
+    cout << "   *All cats have a math teacher." << endl;
+
+    cout << "------------------------" << endl;
+
+    cout << "What is your name? ";
+    cin >> userName;   //Get username
+    cout << "Hello, " << userName << "!" << endl;   //Greet user with name
+
+    cout << endl;
+    cout << "What is 6 + 7 = ";
+    cin >> answer;   //Get answer
+
+    cout << "V1 doesn't check the answer. Stay tuned for V2." << endl;
+
+    return 0;
+}
